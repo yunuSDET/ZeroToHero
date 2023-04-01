87124cbe43993773ef001e0d265601abd0bbdb52
yunuskulcu84 <yunuskulcu84@gmail.com>
2023-04-01 22:16:37 +0300

    First Commit

---
+++ src/day11/Ex7.java
@@ -0,0 +1,29 @@
+package day11;
+
+public class Ex7 {
+
+    public static void main(String[] args) {
+
+        //*Print the following shape with only one line code:
+        //* * * * * * * * * *
+        //* * * * * * * * * *
+        //* * * * * * * * * *
+        //* * * * * * * * * *
+        //* * * * * * * * * *
+        //* * * * * * * * * *
+        //* * * * * * * * * *
+        //* * * * * * * * * *
+        //* * * * * * * * * *
+        //* * * * * * * * * *
+
+        System.out.println(("* ".repeat(10)+"\n").repeat(10));
+
+
+
+
+
+    }
+
+
+
+}
