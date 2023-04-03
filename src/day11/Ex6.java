c4ad9e7b97474295dc5d37a128564d846c2246fa
yunuskulcu84 <yunuskulcu84@gmail.com>
2023-04-03 22:26:23 +0300

    First Commit

--- src/day11/Ex6.java
+++ src/day11/Ex6.java
@@ -4,7 +4,32 @@ public class Ex6 {
 
     public static void main(String[] args) {
 
-      //TODO GÖREV YAPMAMIŞSIN
+    //Convert all characters  except letters and
+    // numbers from a string to spaces
+
+
+        //First solution
+        String text="Hi, my name is Yunus. What is your name? I am 38 years old.";
+        String result="";
+        for (int i = 0; i < text.length(); i++) {
+            char ch=text.charAt(i);
+            if((ch>='a' && ch<='z') ||(ch>='A' && ch<='Z') || (ch>='0' && ch<='9') ){
+              result+=ch;
+            }else{
+                result+=" ";
+            }
+        }
+        System.out.println(result);
+
+
+
+        //Second solution
+        System.out.println(text.replaceAll("[^0-9A-Za-z]", " "));
+
+
+
+
+
 
 
     }
---
+++ src/day12/Ex5.java
@@ -0,0 +1,13 @@
+package day12;
+
+public class Ex5 {
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+}
